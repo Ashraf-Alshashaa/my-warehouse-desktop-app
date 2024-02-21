9be71d89285b7af2c635ac04a083c3bad6c4e624
ashraf alshashaa <ashraf.alchechae@gmail.com>
2024-02-21 08:07:33 +0100

    Imported customtkinter and runned the app

---
+++ app/main.py
@@ -0,0 +1,19 @@
+try:
+    from customtkinter import * 
+
+    set_appearance_mode("System")  
+
+    set_default_color_theme("blue")  
+
+    app = CTk()  
+
+    app.title("My Warehouse")
+
+    app.iconbitmap("../resources/images/app.ico")
+    
+    app.after(0, lambda: app.state('zoomed'))
+
+    app.mainloop()
+
+except Exception as e:
+    print(f"An error occurred: {e}")
