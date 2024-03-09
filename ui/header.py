627c2c67b655188e482883763edafffa53706e2b
ashraf alshashaa <ashraf.alchechae@gmail.com>
2024-03-10 00:22:26 +0100

    Created Sidebar

--- ui/header.py
+++ ui/header.py
@@ -1,14 +1,14 @@
 from . import *
 from time import strftime
 
-def header(parent, page):
+def header(parent):
     header = CTkFrame(parent)
-    header.grid( row=0, column=2, columnspan=22, sticky='nsew',  padx=(5, 20), pady=(20, 5))
+    header.grid( row=0, column=1, sticky='nsew',  padx=(5, 20), pady=(20, 5))
     
-    page_title = CTkLabel(header, text=page, font=("Arial", 24))
+    page_title = CTkLabel(header, text="Create Order", font=("Arial", 24))
     page_title.pack(side="left", padx=(20, 20)) 
 
-    clock = CTkLabel(header, font=("Arial", 16))
+    clock = CTkLabel(header, font=("Arial", 20))
     clock.pack(side="right", padx=(20, 20)) 
     
     def time():
@@ -17,4 +17,9 @@ def header(parent, page):
       clock.after(1000, time)
     
     time()
+
+    return page_title
+
+def update_page_title(label, new_text):
+    label.configure(text=new_text)
     
--- ui/main_window.py
+++ ui/main_window.py
@@ -1,9 +1,15 @@
 from . import *
+from .header import header
+from .sidebar import sidebar
 
 def main_win(parent):
     main_window = CTkFrame(parent)
-    main_window.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23), weight=1, uniform="a")
-    main_window.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), weight=1, uniform="b")
-    main_window.grid( row=0, column=0, sticky='nsew')
+    main_window.grid( row=0, column=0, rowspan=1, columnspan=1, sticky='nsew')
 
-    header(main_window, "Create Order")
+    main_window.grid_columnconfigure(0, weight=1)
+    main_window.grid_columnconfigure(1, weight=100)
+    main_window.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), weight=1, uniform="a")
+   
+    page_title_label = header(main_window)
+
+    sidebar(main_window, page_title_label)
